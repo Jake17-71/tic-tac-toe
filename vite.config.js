ce727de2642758742ad7943b66f2addf84fcd00a
Dan Arhipov <chifmatsuno17@gmail.com>
2025-10-27 16:09:26 +0300

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/tic-tac-toe/',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
